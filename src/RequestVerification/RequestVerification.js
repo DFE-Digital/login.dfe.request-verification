98cd3fea7a4c088db6eb328f9ead0fbea5b75e18
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-11-28 10:22:43 +0000

    Update test file for removing classes

--- src/RequestVerification/RequestVerification.js
+++ src/RequestVerification/RequestVerification.js
@@ -1,34 +1,28 @@
 'use strict';
 
 const fs = require('fs');
-const DigitalSignatureService = require('./../Utils/DigitalSignatureService');
+const digitalSignatureService = require('./../Utils/DigitalSignatureService');
 
-let digitalSignatureService;
 
-class RequestVerification {
-  constructor() {
-    digitalSignatureService = new DigitalSignatureService();
+const isFileSync = (aPath) => {
+  try {
+    return fs.statSync(aPath).isFile();
+  } catch (e) {
+    return false;
+  }
+};
+
+const verifyRequest = (contents, cert, sig) => {
+  let publicKey = cert;
+
+  if (isFileSync(cert)) {
+    publicKey = fs.readFileSync(cert, 'utf8');
   }
 
-  isFileSync(aPath) {
-    try {
-      return fs.statSync(aPath).isFile();
-    } catch (e) {
-      return false;
-    }
-  }
+  const verified = digitalSignatureService.verifyRequest(contents, publicKey, sig);
+  return verified;
+};
 
-  verifyRequest(contents, cert, sig) {
-
-    let publicKey = cert;
-
-    if(this.isFileSync(cert)){
-      publicKey = fs.readFileSync(cert, 'utf8');
-    }
-
-    const verified = digitalSignatureService.verifyRequest(contents, publicKey, sig);
-    return verified;
-  }
-
-}
-module.exports = RequestVerification;
+module.exports = {
+  verifyRequest,
+};
